1b079ecf2ec83cca8f0bed176b51b2c19b3a3893
williamson9t9 <132400959+williamson9t9@users.noreply.github.com>
2023-05-02 23:57:06 +0300

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
+					endpoint.replace(/(\/\/)/, '/') + encodeURI(path as string)
 				}`,
 			},
 		};
